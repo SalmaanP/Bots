5ed3f5b150cde810387b7c23cca7f0dc30b22e2e
HunkDivine <salmaanpehlari@gmail.com>
2014-03-17 14:45:06 +0530

    Final Commit
    version 1.0

--- hl3.py
+++ hl3.py
@@ -1,10 +1,11 @@
 import praw
 from time import sleep
+import re
 
 #login
 
-r = praw.Reddit(user_agent='example')
-r.login('bottestbot','bottestbot')
+r = praw.Reddit(user_agent='hl3_confirmer_bot by u/sallurocks version 1.0')
+r.login('Enter User Name','Enter Password')
 
 
 
@@ -16,9 +17,11 @@ r.login('bottestbot','bottestbot')
 
 
 
+condition = True
+msg="Thank you For Confirming Half-Life 3,Your Outstanding Logical And Mathematical Skills Will Forever Be Remembered.\n\n\nYour Confirmation Is Being Confirmed For Future Reference\n\n______________________________\n\nCurrent Confirmation Count:"
+keyword={'hl3 confirmed','hl3 confirm','hl confirm','hl 3 confirm','hl confirmed','hl 3 confirmed','half life confirm','half life 3 confirm','half-life 3 confirmed','halflife3 confirmed','half life3 confirm','half-life3 confirm'}
 
-
-while True:
+while condition:
 
     #a+ to append everytime
     fo=open("test.txt","a+")
@@ -37,25 +40,30 @@ while True:
 
     #submissions = r.get_submission(submission_id='1z6x8s')
     #flat_comments = praw.helpers.flatten_tree(submissions.comments)
-    subreddit = r.get_subreddit('BOTTESTBOT')
+    subreddit = r.get_subreddit('gaming+dota2+steam+HalfLife')
     flat_comments = subreddit.get_comments()
+
     for comment in flat_comments:
-        if(('HL3 confirmed' in comment.body) and (comment.id not in str)):
-            print('Ok!')
-            print(comment.body)
-            count=count+1
-            comment.reply("Alright Noted count:%s" % count)
-            fo.write(comment.id+" ")
-            f1.seek(0,0)
-            f1.write("%s" % count)
+        for value in keyword:
             
-        else:
-            print("No")
-            print(comment.body)
-            sleep(30)
+            if( ( value in comment.body.lower() ) and (comment.id not in str)):
+
+                print('Ok!')
+                print(comment.body)
+                count=count+1
+                comment.reply(msg+"%s" % count)
+                fo.write(comment.id+" ")
+                f1.seek(0,0)
+                f1.write("%s" % count)
+    
+            else:
+                print("No")
+                print(comment.body)
+            
 
     fo.close()
     f1.close()
+    sleep(5)
     
 
 
